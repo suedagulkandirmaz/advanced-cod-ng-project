c90cf8d090c2e74ca1bbf2769d2daf7b9de8dabe
suedagulkandirmaz <skandirmaz@stud.macromedia.de>
2025-04-30 14:47:57 +0300

    30.04.2025

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,35 +1,37 @@
-import tensorflow as tf
-import PIL import Image
+import TensorFlow as tf
+import PIL 
+import image
+import os
+import sqlite3
+import flask
 
-image_size = (224, 224)
-batch_size = 32
+app = flask(__name__)
+
+classes = ['Tomato', 'Potato', 'Pepper']
+statuses = ['Healthy', 'Diseased']
+
+for plant in classes:
+    for status in statuses:
+        path = os.path.join('data', 'dataset', plant, status)
+        os.makedirs(path, exist_ok=True)
+
+
 
 class Plant:
-    def_init_(self, name):
-    self.name = name
+    def __init__(self, name):
+        self.name = name
 
     def info(self):
-        retur f"{self.name} is not a disease."
+        return f"{self.name} is a healthy."
 
 class DiseasePlant(Plant):
-    def_init_(self,name,disease):
-    super()._init_(name)
-    self.disease = DiseasePlant
+    def __init__(self, name, disease):
+        super().__init__(name)
+    self.disease = disease
 
     def info(self):
         return f"{self.name} has disease: {self.disease}."
 
 
-
-
-from models.plant import Plant 
-
-class User(Person):
-    def__init__(self, person_id, name):
-       super().__init__(person_id, name):
-       self.ansalyzed_plants = []
-    
-    def add_plant_analysis(self, plant: Plant):
-        self.analyzed_plants.append(plant)
 
     
