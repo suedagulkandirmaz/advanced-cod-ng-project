135ab98fc58b1325995fa3cd06b7b477bd2fadb1
suedagulkandirmaz <skandirmaz@stud.macromedia.de>
2025-06-04 13:08:08 +0300

    6.4

--- app/core/dataset_loader.py
+++ app/core/dataset_loader.py
@@ -1,8 +1,19 @@
 # data_loader.py
-
+import os
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import Compose, Resize, ToTensor
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize
 from torch.utils.data import DataLoader
+
+class PlantDiseaseDataLoader:
+    def __init__(self, root_path, batch_size=32):
+        self.root_path = root_path
+        self.batch_size = batch_size
+        self.preprocess = Compose([
+            Resize((self.img_dim, self.img_dim)),
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406],
+                      std=[0.229, 0.224, 0.225])
+        ])
 
 def get_data_loader(directory_path, batch_size=32):
     preprocessing_steps = Compose([
